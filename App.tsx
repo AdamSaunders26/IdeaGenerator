e65696d6e306ebaa302dd28a50183d9805c17791
adamsaunders26 <a.saunders206@gmail.com>
2023-09-05 16:41:29 +0100

    first commit, react native + tailwind set up

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View } from "react-native";
+import Styled from "./app/Styled";
+
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-pink-500">
+      <Text>Open up App.js to start working on your app!</Text>
+      <Styled />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/Styled.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Styled() {
+  return <Text className="text-red-500">This is some text</Text>;
+}

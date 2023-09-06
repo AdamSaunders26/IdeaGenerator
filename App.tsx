3f9dba5d1a49617c5b5089a4eb2372bc71553145
adamsaunders26 <a.saunders206@gmail.com>
2023-09-06 13:36:25 +0100

    add styling and layout for home page

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import Styled from "./app/Styled";
+import ADIcon from "react-native-vector-icons/AntDesign";
 
 import { NativeWindStyleSheet } from "nativewind";
 
@@ -11,10 +12,19 @@ NativeWindStyleSheet.setOutput({
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-pink-500">
-      <Text>Open up App.js to start working on your app!</Text>
-      <Styled />
-      <StatusBar style="auto" />
+    <View className="flex flex-col  px-8 justify-between py-8 pt-14 bg-white h-full ">
+      <Text className="  font-bold text-3xl ">App Idea Generator</Text>
+      <View className="items-center my-32 mx-16 py-4 rounded-full justify-center bg-green-500">
+        <ADIcon name="questioncircleo" size={200} />
+      </View>
+      <Text className="text-lg mt-16">
+        Click the button below to generate 5 app ideas.
+      </Text>
+      <View className="bg-green-500  p-4  px-6 rounded-full flex flex-row items-center justify-between">
+        <Text className="font-bold text-3xl">Generate</Text>
+        <ADIcon name="arrowright" size={50} />
+      </View>
+      <StatusBar style="light" />
     </View>
   );
 }
